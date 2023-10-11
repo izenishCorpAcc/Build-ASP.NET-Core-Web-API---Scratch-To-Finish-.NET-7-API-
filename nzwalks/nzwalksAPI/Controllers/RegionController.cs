787696c47ae1244f96337c7c4eec9371bd3e2a9e
Jenish Prajapati <jenish.prajapati@cotiviti.com>
2023-10-11 15:08:04 +0545

    Using DBcontext to obtain the data

--- nzwalks/nzwalksAPI/Controllers/RegionController.cs
+++ nzwalks/nzwalksAPI/Controllers/RegionController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using nzwalks.API.Data;
 using nzwalks.API.Models.Domain;
 
 namespace nzwalks.API.Controllers
@@ -7,44 +8,34 @@ namespace nzwalks.API.Controllers
     [ApiController]
     public class RegionController : Controller
     {
-        private readonly List<Region> regions = new List<Region>
-            {
-                new Region
-                {
-                    id=Guid.NewGuid(),
-                    Name="PoonHill",
-                    Code="PHL",
-                    RegionImageUrl="https://th.bing.com/th/id/OIP.1xTcEJX8S_RFHKnl_fGavQHaEo?w=296&h=185&c=7&r=0&o=5&pid=1.7"
-                },
-                   new Region
-                {
-                    id=Guid.NewGuid(),
-                    Name="Ghandruk",
-                    Code="GRK",
-                    RegionImageUrl="https://th.bing.com/th/id/OIP.EafU5y43CfLKskEA-TJdjgHaFj?w=249&h=187&c=7&r=0&o=5&pid=1.7"
-                },
-            };
+        private readonly nzwalksdbcontext _dbcontext;
+        public RegionController(nzwalksdbcontext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+        //private readonly List<Region> regions = new List<Region>
+        //    {
+        //        new Region
+        //        {
+        //            id=Guid.NewGuid(),
+        //            Name="PoonHill",
+        //            Code="PHL",
+        //            RegionImageUrl="https://th.bing.com/th/id/OIP.1xTcEJX8S_RFHKnl_fGavQHaEo?w=296&h=185&c=7&r=0&o=5&pid=1.7"
+        //        },
+        //           new Region
+        //        {
+        //            id=Guid.NewGuid(),
+        //            Name="Ghandruk",
+        //            Code="GRK",
+        //            RegionImageUrl="https://th.bing.com/th/id/OIP.EafU5y43CfLKskEA-TJdjgHaFj?w=249&h=187&c=7&r=0&o=5&pid=1.7"
+        //        },
+        //    };
+
         [HttpGet]
         public IActionResult Getall()
         {
-            //var result = new List<Region>
-            //{
-            //    new Region
-            //    {
-            //        id=Guid.NewGuid(),
-            //        Name="PoonHill",
-            //        Code="PHL",
-            //        RegionImageUrl="https://th.bing.com/th/id/OIP.1xTcEJX8S_RFHKnl_fGavQHaEo?w=296&h=185&c=7&r=0&o=5&pid=1.7"
-            //    },
-            //       new Region
-            //    {
-            //        id=Guid.NewGuid(),
-            //        Name="Ghandruk",
-            //        Code="GRK",
-            //        RegionImageUrl="https://th.bing.com/th/id/OIP.EafU5y43CfLKskEA-TJdjgHaFj?w=249&h=187&c=7&r=0&o=5&pid=1.7"
-            //    },
-            //};
-            var result=regions.ToList();
+           
+            var result=_dbcontext.Regions.ToList();
             return Ok(result);
 
         }
@@ -52,7 +43,7 @@ namespace nzwalks.API.Controllers
         [HttpGet("{code}")]
         public IActionResult Get(string code)
         {
-            var result= regions.FirstOrDefault(r=>r.Code==code);
+            var result= _dbcontext.Regions.FirstOrDefault(r=>r.Code==code);
             if(result == null)
             {
                 return NotFound();
